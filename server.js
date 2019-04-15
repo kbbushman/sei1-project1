16b843253735027b25efa89975545c522110eb67
kbbushman <k.bushman@yahoo.com>
2019-04-15 10:00:44 -0700

    Initial commit adds server boilerplate

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Serve Static Assets
+app.use(express.static(__dirname + '/public'));
+
+// ROOT Route
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', {root: __dirname});
+});
+
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
