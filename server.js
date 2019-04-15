3fc13a564de7464d09213648473d3ad398769574
DaltonHart <DaltonVaren@gmail.com>
2019-04-15 10:22:31 -0700

    added api route for documentation

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const db = require('./models');
 
 // BodyParser Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve Static Assets
@@ -15,9 +15,23 @@ app.use(express.static(__dirname + '/public'));
 
 // ROOT Route
 app.get('/', (req, res) => {
-  res.sendFile('views/index.html', {root: __dirname});
+	res.sendFile('views/index.html', { root: __dirname });
 });
 
+// Documentation Route for API
+app.get('/api', (req, res) => {
+	res.json({
+		documentationUrl: 'https://github.com/kbbushman/sei1-project1',
+		// List all available endpoints for the API
+		endpoints: [
+			{
+				method: 'GET',
+				path: '/api',
+				description: 'Describes all available endpoints'
+			}
+		]
+	});
+});
 
 // Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
